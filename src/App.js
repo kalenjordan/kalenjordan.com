717956f6513db516ecf627572d4b3bcba6b3bd2b
Kalen Jordan <kalen@kalens-laptop.lan>
2024-04-27 08:05:19 -0500

    Install prettier

--- src/App.js
+++ src/App.js
@@ -31,21 +31,7 @@ function App() {
             description="Discover how Shopify can power up your entrepreneurial journey."
             popoverActions={[{content: 'Dismiss', onAction: () => {}}]}
           >
-            <img
-              alt=""
-              width="100%"
-              height="100%"
-              style={{
-                objectFit: 'cover',
-                objectPosition: 'center',
-              }}
-              src="https://burst.shopifycdn.com/photos/business-woman-smiling-in-office.jpg?width=1850"
-            />
           </MediaCard>
-          <Card>
-            
-          </Card>
-
         </BlockStack>
         <FooterHelp>
           <InlineStack gap="200">
