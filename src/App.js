c05f831ee4a9ed3ecc5db7e8f50d25eba95de490
Kalen Jordan <kalen@kalens-laptop.lan>
2024-04-28 07:53:30 -0500

    Add rates

--- src/App.js
+++ src/App.js
@@ -3,45 +3,80 @@ import "./App.css";
 import enTranslations from "@shopify/polaris/locales/en.json";
 import {
   AppProvider,
-  Page,
-  Grid,
-  Text,
-  TextField,
+  Bleed,
+  Box,
+  Button,
   Card,
-  MediaCard,
+  Grid,
+  Page,
+  Text,
   Thumbnail,
   InlineStack,
-  Button,
   FooterHelp,
   Link,
-  Toast,
   Frame,
-  Scrollable,
   BlockStack,
 } from "@shopify/polaris";
 import "@shopify/polaris/build/esm/styles.css";
-import { useState, useCallback } from "react";
-import { useLocalStorage } from "./useLocalStorage";
-import { ClipboardIcon } from "@shopify/polaris-icons";
+import { CartIcon } from "@shopify/polaris-icons";
 
 function App() {
   return (
     <AppProvider i18n={enTranslations}>
-      <video width="100%" autoplay="autoplay" loop="loop">
-        <source src="flow-hire.mp4" />
-      </video>
       <Frame>
-        <Page>
+        <Page
+          title="Shopify Automation With A Smile 😀"
+          subtitle="I love building Shopify integrations. Whether using Flow, IPaaS systems, or building custom - if you need any integrations built for Shopify or other systems you integrate with, I'm your guy."
+        >
           <BlockStack gap="500">
-            <MediaCard
-              title="Shopify automation with a smile"
-              primaryAction={{
-                content: "Learn about getting started",
-                onAction: () => {},
-              }}
-              description="Discover how Shopify can power up your entrepreneurial journey."
-              popoverActions={[{ content: "Dismiss", onAction: () => {} }]}
-            ></MediaCard>
+            <Card>
+              <Bleed marginInline="400" marginBlock="400">
+                <video width="100%" autoPlay="autoplay" loop="loop">
+                  <source src="flow-hire.mp4" />
+                </video>
+              </Bleed>
+            </Card>
+            <Grid columns={{ sm: 3 }}>
+              <Grid.Cell columnSpan={{ xs: 6, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                <Card>
+                  <BlockStack gap="300" as="lfdsafdsa">
+                    <Text as="h2" variant="headingSm">
+                      10 Hours
+                    </Text>
+                    <Text as="p" variant="bodyMd">
+                      Tall
+                    </Text>
+                    <Button icon={CartIcon}>$1200</Button>
+                  </BlockStack>
+                </Card>
+              </Grid.Cell>
+              <Grid.Cell columnSpan={{ xs: 6, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                <Card>
+                  <BlockStack gap="300">
+                    <Text as="h2" variant="headingSm">
+                      20 Hours
+                    </Text>
+                    <Text as="p" variant="bodyMd">
+                      Grande
+                    </Text>
+                    <Button icon={CartIcon}>$2100</Button>
+                  </BlockStack>
+                </Card>
+              </Grid.Cell>
+              <Grid.Cell columnSpan={{ xs: 6, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                <Card>
+                  <BlockStack gap="300">
+                    <Text as="h2" variant="headingSm">
+                      30 Hours
+                    </Text>
+                    <Text as="p" variant="bodyMd">
+                      Venti
+                    </Text>
+                    <Button icon={CartIcon}>$3000</Button>
+                  </BlockStack>
+                </Card>
+              </Grid.Cell>
+            </Grid>
           </BlockStack>
           <FooterHelp>
             <InlineStack gap="200">
