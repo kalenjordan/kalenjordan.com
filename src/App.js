f89df665f5ddc0095a5dcf456ee5f1e11c8b6f29
Kalen Jordan <kalen@kalens-laptop.lan>
2024-04-30 14:28:14 -0500

    Add youtube video

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   Thumbnail,
   InlineStack,
   FooterHelp,
+  Layout,
   Link,
   Frame,
   BlockStack,
@@ -35,132 +36,157 @@ function App() {
           <BlockStack gap="500">
             <Card>
               <Bleed marginInline="400" marginBlock="400">
-                <video width="100%" autoPlay="autoplay" loop="loop">
+                <video width="100%" autoPlay="autoplay" loop="loop" controls>
                   <source src="flow-hire.mp4" />
                 </video>
               </Bleed>
             </Card>
-            <Grid columns={{ sm: 3 }}>
-              <Grid.Cell columnSpan={{ xs: 6, sm: 4, md: 2, lg: 8, xl: 8 }}>
-                <Card>
-                  <BlockStack gap="300">
-                    <Text as="h2" variant="headingSm">
-                      Services
-                    </Text>
+            <Layout>
+              <Layout.Section>
+                <BlockStack gap="500">
+                  <Card title="Order details" sectioned>
+                    <BlockStack gap="300">
+                      <Text as="h2" variant="headingSm">
+                        Services
+                      </Text>
 
-                    <Text as="p" variant="bodyMd">
-                      Having built hundreds of workflow automations for Shopify
-                      using 3rd party apps, Flow, and custom code, if there's
-                      something that's possible to do in Shopify, I should be
-                      able to do it for you. And usually pretty quickly.
-                    </Text>
-                    <Text as="p" variant="bodyMd">
-                      I can also understand requirements and communicate well,
-                      so that you won't have to bang your head against the wall
-                      trying to articulate exactly what needs to happen and
-                      every potential edge case that may need to be handled.
-                      I'll figure it out.
-                    </Text>
-                  </BlockStack>
-                </Card>
-              </Grid.Cell>
-              <Grid.Cell columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}>
-                <Card>
-                  <BlockStack gap="300">
-                    <InlineStack gap="200">
-                      <Avatar size="xs" source="flow-helper-icon.png" />
-                      <Text as="h2" variant="headingSm">
-                        Flow Helper Shopify App
+                      <Text as="p" variant="bodyMd">
+                        Having built hundreds of workflow automations for
+                        Shopify using 3rd party apps, Flow, and custom code, if
+                        there's something that's possible to do in Shopify, I
+                        should be able to do it for you. And usually pretty
+                        quickly.
                       </Text>
-                    </InlineStack>
-                    <Text as="p" variant="bodyMd">
-                      Flow Helper is a Shopify app that helps you build more
-                      powerful workflows with Shopify Flow by adding more
-                      triggers and actions, such as product, customer, and order
-                      update triggers.
-                    </Text>
-                    <span>
-                      <Badge as="span" tone="info">
-                        Pending Review
-                      </Badge>
-                    </span>
-                  </BlockStack>
-                </Card>
-              </Grid.Cell>
-            </Grid>
-            <Grid columns={{ sm: 3 }}>
-              <Grid.Cell columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}>
-                <Card>
-                  <BlockStack gap="300" as="lfdsafdsa">
-                    <Text as="h2" variant="headingSm">
-                      10 Hours
-                    </Text>
-                    <Text as="p" variant="bodyMd">
-                      With 10 hours of consulting, let's get off to the races
-                      with a quick win.
-                    </Text>
-                    <span>
-                      <Button
-                        icon={CartIcon}
-                        url="https://buy.stripe.com/dR601u0mX4eP9vafZ4"
-                        target="_blank"
-                      >
-                        $1,200
-                      </Button>
-                    </span>
-                  </BlockStack>
-                </Card>
-              </Grid.Cell>
-              <Grid.Cell columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}>
-                <Card>
-                  <BlockStack gap="300">
-                    <Text as="h2" variant="headingSm">
-                      20 Hours
-                    </Text>
-                    <Text as="p" variant="bodyMd">
-                      With 20 hours of consulting, we'll have a little more time
-                      to get things just right.
-                    </Text>
-                    <span>
-                      <Button
-                        url="https://buy.stripe.com/7sI15yedNeTtazeeV1"
-                        target="_blank"
-                        icon={CartIcon}
-                      >
-                        $2,100
-                      </Button>
-                    </span>
-                  </BlockStack>
-                </Card>
-              </Grid.Cell>
-              <Grid.Cell columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}>
-                <Card>
-                  <BlockStack gap="300">
-                    <InlineStack gap="200">
-                      <Text as="h2" variant="headingSm">
-                        30 Hours
+                      <Text as="p" variant="bodyMd">
+                        I can also understand requirements and communicate well,
+                        so that you won't have to bang your head against the
+                        wall trying to articulate exactly what needs to happen
+                        and every potential edge case that may need to be
+                        handled. I'll figure it out.
                       </Text>
-                      <Badge gap="500" tone="success">
-                        Best Value
-                      </Badge>
-                    </InlineStack>
-                    <Text as="p" variant="bodyMd">
-                      With a 30 hour consulting package, we're ready to start
-                      cooking with gas.
-                    </Text>
-                    <span>
-                      <Button
-                        url="https://buy.stripe.com/28oaG85Hh3aL6iY3ch"
-                        target="_blank"
-                        icon={CartIcon}
+                      <Text as="p" variant="bodyMd">
+                        If you're looking for help with anything along these
+                        lines, I can't wait to talk to you!
+                      </Text>
+                    </BlockStack>
+                  </Card>
+                  <Card>
+                    <Grid columns={{ sm: 3 }}>
+                      <Grid.Cell
+                        columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}
                       >
-                        $3,000
-                      </Button>
-                    </span>
-                  </BlockStack>
-                </Card>
-              </Grid.Cell>
-            </Grid>
+                        <BlockStack gap="300" as="lfdsafdsa">
+                          <Text as="h2" variant="headingSm">
+                            10 Hours
+                          </Text>
+                          <Text as="p" variant="bodyMd">
+                            With 10 hours of consulting, let's get off to the
+                            races with a quick win.
+                          </Text>
+                          <span>
+                            <Button
+                              icon={CartIcon}
+                              url="https://buy.stripe.com/dR601u0mX4eP9vafZ4"
+                              target="_blank"
+                            >
+                              $1,200
+                            </Button>
+                          </span>
+                        </BlockStack>
+                      </Grid.Cell>
+                      <Grid.Cell
+                        columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}
+                      >
+                        <BlockStack gap="300">
+                          <Text as="h2" variant="headingSm">
+                            20 Hours
+                          </Text>
+                          <Text as="p" variant="bodyMd">
+                            With 20 hours of consulting, we'll have a little
+                            more time to get things just right.
+                          </Text>
+                          <span>
+                            <Button
+                              url="https://buy.stripe.com/7sI15yedNeTtazeeV1"
+                              target="_blank"
+                              icon={CartIcon}
+                            >
+                              $2,100
+                            </Button>
+                          </span>
+                        </BlockStack>
+                      </Grid.Cell>
+                      <Grid.Cell
+                        columnSpan={{ xs: 6, sm: 4, md: 1, lg: 4, xl: 4 }}
+                      >
+                        <BlockStack gap="300">
+                          <InlineStack gap="200">
+                            <Text as="h2" variant="headingSm">
+                              30 Hours
+                            </Text>
+                            <Badge gap="500" tone="success">
+                              Best Value
+                            </Badge>
+                          </InlineStack>
+                          <Text as="p" variant="bodyMd">
+                            With a 30 hour consulting package, we're ready to
+                            start cooking with gas.
+                          </Text>
+                          <span>
+                            <Button
+                              url="https://buy.stripe.com/28oaG85Hh3aL6iY3ch"
+                              target="_blank"
+                              icon={CartIcon}
+                            >
+                              $3,000
+                            </Button>
+                          </span>
+                        </BlockStack>
+                      </Grid.Cell>
+                    </Grid>
+                  </Card>
+                </BlockStack>
+              </Layout.Section>
+              <Layout.Section variant="oneThird">
+                <BlockStack gap="500">
+                  <Card title="Tags" sectioned>
+                    <BlockStack gap="300">
+                      <InlineStack gap="200">
+                        <Avatar size="xs" source="flow-helper-icon.png" />
+                        <Text as="h2" variant="headingSm">
+                          Flow Helper Shopify App
+                        </Text>
+                      </InlineStack>
+                      <Text as="p" variant="bodyMd">
+                        A Shopify app that adds missing triggers and actions to
+                        use natively within Flow, including bulk flow runs.
+                      </Text>
+                      <span>
+                        <Badge as="span" tone="info">
+                          Pending Review
+                        </Badge>
+                      </span>
+                    </BlockStack>
+                  </Card>
+                  <Card>
+                    <Bleed marginInline="400" marginBlock="400">
+                      <div className="video-container">
+                        <iframe
+                          width="300"
+                          height="170"
+                          src="https://www.youtube.com/embed/U9jsIxIb-k8?si=bOHL8TKMG7eZ5h60"
+                          title="YouTube video player"
+                          frameborder="0"
+                          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                          referrerpolicy="strict-origin-when-cross-origin"
+                          allowfullscreen
+                        ></iframe>
+                      </div>
+                    </Bleed>
+                  </Card>
+                </BlockStack>
+              </Layout.Section>
+            </Layout>
             <FooterHelp>
               <BlockStack gap="500">
                 <BlockStack gap="100">
